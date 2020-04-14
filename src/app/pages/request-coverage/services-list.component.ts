2e292f2ee84cb1be6434a43cd0d01cde236b7b5a
Jade <wangyujue19960222@gmail.com>
2020-04-14 18:39:58 -0400

    enable post method

--- src/app/pages/request-coverage/services-list.component.ts
+++ src/app/pages/request-coverage/services-list.component.ts
@@ -1,39 +1,43 @@
 import { Component } from '@angular/core';
 
 import { services } from './services';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   templateUrl: './services-list.component.html',
   styleUrls: ['./services-list.component.scss']
 })
 export class ServiceListComponent {
+  constructor (private http:HttpClient) {}
   services = services;
 
   model = new services();
 
-  service_tier = 0;
-
   selectT1() {
     var modal = document.getElementById("popup");
     modal.style.display = "block";
-    this.service_tier = 1;
   }
 
   selectT2() {
     var modal = document.getElementById("popup");
     modal.style.display = "block";
-    this.service_tier = 2;
   }
 
   selectT3() {
     var modal = document.getElementById("popup");
     modal.style.display = "block";
-    this.service_tier = 3;
   }
 
   close() {
     var modal = document.getElementById("popup");
     modal.style.display = "none";
+  }
+
+  onSubmit(data) {
+    this.http.post("http://localhost:3000/rampart/request_coverage/insert", data)
+    .subscribe((result)=>{
+        console.warn("result", result)
+    })
   }
 
   handleSubmit() {
