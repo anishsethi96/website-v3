3f65e98907bfeeb6a4c314ad1ccd96d723135dc1
Jade <wangyujue19960222@gmail.com>
2020-04-14 17:27:06 -0400

    get form data done

--- src/app/pages/request-coverage/services-list.component.ts
+++ src/app/pages/request-coverage/services-list.component.ts
@@ -8,24 +8,42 @@ import { services } from './services';
 })
 export class ServiceListComponent {
   services = services;
-  
+
+  model = new services();
+
+  service_tier = 0;
+
   selectT1() {
     var modal = document.getElementById("popup");
     modal.style.display = "block";
+    this.service_tier = 1;
   }
 
   selectT2() {
     var modal = document.getElementById("popup");
     modal.style.display = "block";
+    this.service_tier = 2;
   }
 
   selectT3() {
     var modal = document.getElementById("popup");
     modal.style.display = "block";
+    this.service_tier = 3;
   }
 
   close() {
     var modal = document.getElementById("popup");
     modal.style.display = "none";
   }
+
+  handleSubmit() {
+    const form = document.querySelector('form');
+    const data = new FormData(form);
+    // Need the backend information here
+  }
+
+  clearForm() {
+    this.model = new services();
+  }
 }
+
